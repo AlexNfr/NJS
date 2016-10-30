edc55599034765eec856c870038276a7467d202c
AlexNfr <nfrcaptain@yandex.ru>
2016-10-30 21:05:51 +0300

    Сделано ДЗ № 3.

--- homework-3/sk-news.js
+++ homework-3/sk-news.js
@@ -3,15 +3,31 @@
  */
 
 var request = require('request'),
-    http = require('http'),
+    // http = require('http'),
     cheerio = require('cheerio');
 var URL_TO_PARSE = 'http://sk.ru/news/b/news';
+var titles = [],
+    reviews = [],
+    dates = [];
+
+console.log('Skovkovo News Parser ver. 0.1 (c) 2016 by AlexNfr\n\n');
 
 request(URL_TO_PARSE, function (error, response, html) {
-    if (!error && response.ststusCode == 200) {
-        response.writeHead(200,{'Content-Type': 'text/plain; charset=utf8'});
+    if (!error && response.statusCode == 200) {
+        response.setEncoding('utf8');
         var $ = cheerio.load(html);
-        var titles = $('.subject');
-        console.log(titles);
+        $('.subject > h4').each(function(i, element) {
+            titles[i] = $(element).text().trim();
+        });
+        $('.list-item-excerpt').each(function(i, element) {
+            reviews[i] = $(element).text().trim();
+        });
+        $('.list-item-detail-info').each(function(i, element) {
+            dates[i] = $(element).text().trim();
+        });
+        n = titles.length;
+        for (var i = 0; i < n; i++) {
+            console.log('News(', i + 1, '): "', titles[i], '"\n"', reviews[i], '"\n', dates[i], '\n\n');
+        }
     }
 });
