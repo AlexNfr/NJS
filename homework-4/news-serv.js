19f3e314ae5bbb0af0e5d539ffa5fb8c63dff46e
AlexNfr <nfrcaptain@yandex.ru>
2016-11-02 19:20:33 +0300

    Начато ДЗ № 4 (сделана задача 2).

--- homework-4/news-serv.js
+++ homework-4/news-serv.js
@@ -3,15 +3,64 @@
  */
 
 var express = require('express');
-    app = express();
 var templating = require('consolidate');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var port = Number(process.env.port || 3200);
+var app = express();
+
+function getFilterSettings(req) {
+    var filter = {};
+
+    filter[req.cookies.newsservcategory] = 'selected';
+    filter[req.cookies.newsservsize] = 'selected';
+    return filter;
+}
+
+function putFilterSettings(req, res) {
+    res.cookie('newsservcategory', (req.body['news-category'] || 'fresh'));
+    res.cookie('newsservsize', (req.body['news-size'] || 10));
+}
+
+function getNewsList(category, size) {
+    var newsList = 'Единственная новость на сегодня: плохих новостей нет';
+
+    return newsList;
+}
 
 app.engine('hbs', templating.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+app.use(bodyParser());
+app.use(cookieParser());
+
 app.get('/', function (req, res) {
-    res.render('hello', {
-        title: 'Привет, handlebars!'
+    res.render('news', {
+        title: 'Сервер новостей Сколково приветствует вас!',
+        message: 'Здесь вы можете узнать все новости из Сколково. '
+                + 'Для этого достаточно выбрать желаемые параметры фильтра новостей и нажать кнопку "Показать":',
+        filter: getFilterSettings(req),
     });
+
+});
+
+app.route('/news')
+    .get(function (req, res) {
+        res.render('news', {
+            title: 'Новости Сколково',
+            message: 'Пожалуйста, наслаждайтесь выбранными новостями:',
+            filter: getFilterSettings(req),
+            newsTitle: 'Список выбранных новостей',
+            newsList: getNewsList()
+        })
+    })
+    .post(function (req, res) {
+        putFilterSettings(req, res);
+        res.redirect('/news');
+    });
+
+app.listen(port, function () {
+    console.log('News server listening on port ' + port);
 });
